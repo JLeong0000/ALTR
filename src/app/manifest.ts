cacef2f7b40f13311b22859d0a11ac7cc014c7cc
JLeong0000 <justinchleong@gmail.com>
2025-02-24 17:14:23 +0800

    Updated site logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,21 +3,15 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
 	return {
 		name: "AI Language Tutor",
-		short_name: "ALTi",
+		short_name: "ALTr",
 		start_url: "/",
 		display: "standalone",
 		background_color: "#ffffff",
 		theme_color: "#000000",
 		icons: [
 			{
-				src: "/icon-192x192.png",
-				sizes: "192x192",
-				type: "image/png",
-			},
-			{
-				src: "/icon-512x512.png",
-				sizes: "512x512",
-				type: "image/png",
+				src: "/src/app/favicon.ico",
+				type: "image/ico",
 			},
 		],
 	};
